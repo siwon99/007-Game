f96bbd28cb25653e7c8fbc677307e758877d209e
siwon99 <kimsiwon0707@gmail.com>
2023-04-09 17:04:18 +0900

    Upgrade Code
    Use not top & left -> Use translate

--- main.js
+++ main.js
@@ -5,18 +5,20 @@ const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-document.addEventListener('mousemove', (e) => {
-  const x = e.clientX;
-  const y = e.clientY;
+// load 상태일 때 이미지와 리소스가 준비되어 있기에 load가 될 때 처리하게 함
+addEventListener('load', () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
 
-  vertical.style.left = `${x}px`
-  horizontal.style.top = `${y}px`
+  document.addEventListener('mousemove', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
 
-  target.style.left = `${x}px`
-  target.style.top = `${y}px`
-
-  tag.style.left = `${x}px`
-  tag.style.top = `${y}px`
-
-  tag.innerHTML = `${x}px, ${y}px`
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`;
+    tag.style.transform = `translate(${x}px, ${y}px)`;
+    tag.innerHTML = `${x}px, ${y}px`
+  });
 });
